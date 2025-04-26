63bdffdbaf38de5938d7323cdd3e0264b0312074
Adithya S <adiskashyap4444@gmail.com>
2025-04-26 16:32:37 +0530

    Update Disariumnumbers.py

--- Disariumnumbers.py
+++ Disariumnumbers.py
@@ -1,15 +1,15 @@
-def dis_num(num):
-    n=len(num)
-    total=0
-    num1=int(num)
-    for pos in range(n):
-        digit = int(num[pos])
-        position = pos+1
-        total += digit ** position
-    return total == num1
-a=input("Enter the number")
-valid=dis_num(a)
-if valid==True:
-    print("The number is a Disarium number")
-else:
-    print("The number is not a Disarium number")
+def is_disarium(n):
+    return n == sum(int(d)**i for i, d in enumerate(str(n), 1))
+def first_n_disarium(n):
+    res, num = [], 0
+    while len(res) < n:
+        if is_disarium(num):
+            res.append(num)
+        num += 1
+    return res
+def disarium_between(a, b):
+    return [x for x in range(a, b+1) if is_disarium(x)]
+n = 10
+print(f"First {n} Disarium numbers:\n{first_n_disarium(n)}")
+a, b = 1, 200
+print(f"\nDisarium numbers between {a} and {b}:\n{disarium_between(a, b)}")
