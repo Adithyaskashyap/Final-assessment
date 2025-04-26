08ed4b137e30bcb63653d641f85e055c4191351f
Adithya S <adiskashyap4444@gmail.com>
2025-04-26 16:28:02 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,15 @@
+def print_pattern():
+    for i in range(5): 
+        for j in range(5): 
+            if (i % 2 == 0):  
+                if j == 4:
+                    print(i + 2, end='')  
+                else:
+                    print(i + 1, end='') 
+            else: 
+                if j == 0:
+                    print(i + 2, end='')  
+                else:
+                    print(i + 1, end='') 
+            print() 
+print_pattern()
